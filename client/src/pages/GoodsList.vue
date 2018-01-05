707f859ad5b81b0cb5e80cacd2d1c5ad45337821
Danielhard <335805097@qq.com>
2018-01-05 18:01:03 +0800

    rename

--- client/src/pages/GoodsList.vue
+++ client/src/pages/GoodsList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <nav-bread>
-        <span>热门商品!!!</span>
+        <span>HotList</span>
       </nav-bread>
       <div class="accessory-result-page accessory-page">
         <div class="container">
